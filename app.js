04c9600b6e32cc8520247a1bfd0cf3db4dfeb4dc
vishnuacappella <acappella.studiov@gmail.com>
2022-07-14 11:55:46 +0530

    promise completed

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
-import makeMeAPromise from './index.js'
+import getData from './index.js'
 
-const data = {
-    car: 'BMW',
-    color: 'red',
-    year: '2018',
-    price: '$1,000,000'
+const response = getData();
+
+console.log(response)
+
+const doThis = (data) => {
+    console.log(`do something with this data`, data)
 }
 
-console.log(makeMeAPromise(data));
-console.log(makeMeAPromise(data, 'reject'));
+response.then(doThis)
+
+// console.log(response)
+
+// setTimeout(() => {
+//     console.log(response)
+// })
+// setTimeout(() => {
+//     console.log(response)
+// })
+// setTimeout(() => {
+//     console.log(response)
+// })
+
+
+
+
+// import makeMeAPromise from './index.js'
+
+// const data = {
+//     car: 'BMW',
+//     color: 'red',
+//     year: '2018',
+//     price: '$1,000,000'
+// }
+
+// console.log(makeMeAPromise(data));
+// console.log(makeMeAPromise(data, 'reject'));
+
+// const response = fetch('https://jsonplaceholder.typicode.com/todos');
+// console.log(response)
+
+// setTimeout(() => {
+//     console.log(response)
+// }, 3000)
