def1a8f1ef1fed7d5e79485baea90d32ef09fa50
vishnuacappella <acappella.studiov@gmail.com>
2022-07-24 05:32:21 +0530

    promises workout 01 completd

--- app.js
+++ app.js
@@ -10,19 +10,47 @@
 // console.log(makeMeAPromise(data));
 // console.log(makeMeAPromise(data, 'reject'));
 
-let p = new Promise((resolve, reject) => {
-    let a = 1 + 1;
-    if (a === 2) {
-        resolve('Success');
-    } else {
-        reject('Failed');
-    }
-});
+// let p = new Promise((resolve, reject) => {
+//     let a = 1 + 1;
+//     if (a === 2) {
+//         resolve('Success');
+//     } else {
+//         reject('Failed');
+//     }
+// });
 
-p.then((message) => {
-    console.log('This is the in the then ' + message);
-}).catch((message) => {
-    console.log('This is the in the catch ' + message);
-});
+// p.then((message) => {
+//     console.log('This is the in the then ' + message);
+// }).catch((message) => {
+//     console.log('This is the in the catch ' + message);
+// });
 
 
+
+// Responses
+const userLeft = false;
+const userWatchingCatMe = true;
+
+function watchTutorialPromise() {
+    return new Promise((resolve, reject) => {
+        if (userLeft) {
+            reject({
+                name: 'User Left',
+                message: ':('
+            });
+        } else if (userWatchingCatMe) {
+            reject({
+                name: 'User Watching Cat Me',
+                message: 'WebDevSimplified < Cat'
+            });
+        } else {
+            resolve('Thumbs up and Subscribe');
+        }
+    });
+};
+
+watchTutorialPromise().then((message) => {
+    console.log('Success: ' + message)
+}).catch((error) => {
+    console.log(error.name + ' ' + error.message)
+})
